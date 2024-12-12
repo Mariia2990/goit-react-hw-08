1728bf73ccf6ca5af5978390cf4e020f23c99d61
Maricnka_Lozynska <masyakuk@gmail.com>
2024-12-12 19:28:03 +0200

    change contacts2

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeFilter} from '../../redux/filtersSlice';
 import css from './SearchBox.module.css';
 import { selectFilter } from '../../redux/contactsSlice';
+import { changeFilter } from '../../redux/filters/slice';
 
 const SearchBox = () => {
   const dispatch = useDispatch();
